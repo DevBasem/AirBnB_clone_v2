063daa44d6ab2481f7e608806819b3d5033dae38
DevBasem <engr.basemahmed@gmail.com>
2024-03-23 15:22:09 -0700

    task 8 fx1

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,9 +18,13 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of states"""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states_sorted)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
