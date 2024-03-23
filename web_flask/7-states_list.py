077db948c7ab7a479eb1caadfcbe19bd70ef21fc
DevBasem <engr.basemahmed@gmail.com>
2024-03-23 15:32:49 -0700

    task 8 fx2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
+""" Starts a Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database again at the end of the request."""
+def dispose(exception):
+    """ Remove current session """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of states"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
