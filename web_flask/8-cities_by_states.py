7f35ed8dac0c6389df9907aa960a6e4302b308c7
DevBasem <engr.basemahmed@gmail.com>
2024-03-24 16:31:18 -0700

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,21 +8,21 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Displays a HTML page showing states and their cities."""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-
-    return render_template('8-cities_by_states.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def dispose(exception):
     """Closes the current SQLAlchemy session."""
     storage.close()
+
+
+@app.route('/cities_by_states')
+def states():
+    """Displays a HTML page showing states and their cities."""
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('8-cities_by_states.html', states=states_list)
 
 
 if __name__ == '__main__':
